e9f8a6c77d90b9503522e4eae15e0964b3e04558
luisf <luisf@LAPTOP-ID232HDT>
2021-05-21 14:23:48 -0500

    correcciones

--- laberinto.js
+++ laberinto.js
@@ -6,7 +6,7 @@ class estado {
     }
 }
 
-const MaximoMovimientos = 3; //Numero maximo de movimientos 
+const MaximoMovimientos = 5; //Numero maximo de movimientos 
 const mapa = new Array(
     "...",
     "##.",
@@ -18,40 +18,30 @@ var visitados = new Array(mapaF);
 for (i = 0; i < mapaF; i++) {
     visitados[i] = new Array(mapaC);
 }
-for (i = 0; i < mapaF; i++) {
-    for (j = 0; j < mapaC; j++) {
-        visitados[i][j] = false;
-    }
-}
 
 let cola = new Array();
 var movx = [1, -1, 0, 0];
 var movy = [0, 0, 1, -1];
 var distanciaMin = busqueda(mapaF, mapaC, 0, 0);
-if (distanciaMin == -1) {
-    console.log("error");
-}
-if (MaximoMovimientos >= distanciaMin) {
+
+if (MaximoMovimientos >= distanciaMin && distanciaMin != -1) {
     console.log("Yes");
 } else {
     console.log("No");
 }
 
-
 function busqueda(filas, columnas, x, y) {
     inical = new estado(x, y, 0);
     cola.push(inical);
     while (cola.length != 0) {
-        var estadoActual = new estado(0, 0, 0);
-        estadoActual = cola.shift();
+        var estadoActual = cola.shift();
         if (estadoActual.x == mapaF - 1 && estadoActual.y == mapaC - 1) {
             return estadoActual.d;
         }
-        visitados[x][y] = true;
+        visitados[estadoActual.x][estadoActual.y] = true;
         for (let i = 0; i < 4; i++) {
             var nuevoX = estadoActual.x + movx[i];
             var nuevoY = estadoActual.y + movy[i];
-
             if (nuevoX >= 0 && nuevoX < mapaF && nuevoY >= 0 && nuevoY < mapaC) {
                 if (!visitados[nuevoX][nuevoY] && mapa[nuevoX][nuevoY] != '#') {
                     var estadoNuevo = new estado(nuevoX, nuevoY, estadoActual.d + 1);
